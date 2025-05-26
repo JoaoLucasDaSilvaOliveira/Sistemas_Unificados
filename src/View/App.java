4cfe3553aad8d56e625eb6cd5019feb1ffef87e1
João Lucas <jlso9866@gmail.com>
2025-05-25 22:22:56 -0300

    Atualizando

--- src/View/App.java
+++ src/View/App.java
@@ -1,7 +1,20 @@
 package View;
 
+import Model.EXEPTIONS.InvalidFormatException;
+import Model.Empresa;
+
 public class App {
     public static void main(String[] args) {
-        //begin
+        //testando se o cadastro de empresa funciona
+        boolean entradaValidada = false;
+        //while (!entradaValidada){
+        try{
+            Empresa emp = Empresa.create("12.345.789/0001-01", "Nome", Empresa.Status.ACTIVE);
+            System.out.println(emp);
+            entradaValidada = true;
+        } catch (InvalidFormatException e) {
+            System.out.println(e.getMessage());
+        }
+        //} //HABILITAR APENAS COM ENTRADAS DO USUÁRIO
     }
 }
