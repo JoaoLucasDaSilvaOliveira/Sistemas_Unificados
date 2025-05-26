31fc03cf75f93e8bc1d9d9b1b0855c0ef249dc09
Kaua Fraga <fragakaua@gmail.com>
2025-05-25 22:25:12 -0300

    Criei os metodos searchAll, create e saveList na classe EmpresaDAO

--- src/DAO/OperacoesDAO.java
+++ src/DAO/OperacoesDAO.java
@@ -4,9 +4,10 @@ import java.util.*;
 
 public interface OperacoesDAO<T,K,V> {
     public boolean create (T creating);
-    public List<T> searchAll (T searching);
+    public List<T> searchAll ();
     public T searchByKey (K searching);
     public T searchByValue (V searching);
     public boolean delete (T deleting);
     public boolean update (T updating);
+    public boolean saveList (List<T> savingList);
 }
