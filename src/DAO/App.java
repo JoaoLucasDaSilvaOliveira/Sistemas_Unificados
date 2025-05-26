4326ac213d05bd867bf107f0f9954ce4d5cc3b9e
João Lucas <jlso9866@gmail.com>
2025-05-25 22:20:32 -0300

    Testado feature

--- src/DAO/App.java
+++ src/DAO/App.java
@@ -1,12 +1,21 @@
 package DAO;
 
+import Model.EXEPTIONS.InvalidFormatException;
 import Model.Empresa;
 import Model.Empresa.Status;
 
 public class App {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) {
         //testando se o cadastro de empresa funciona
-        Empresa emp = Empresa.create("12.345.789/0001-01", "Nome", Status.ACTIVE);
-        System.out.println(emp);
+        boolean entradaValidada = false;
+        //while (!entradaValidada){
+            try{
+                Empresa emp = Empresa.create("12.345.789/0001-01", "Nome", Status.ACTIVE);
+                System.out.println(emp);
+                entradaValidada = true;
+            } catch (InvalidFormatException e) {
+                System.out.println(e.getMessage());
+            }
+        //} //HABILITAR APENAS COM ENTRADAS DO USUÁRIO
     }
 }
